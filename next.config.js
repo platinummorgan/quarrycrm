9bd27fade4e80de8860842bd64300e7eadaf9f29
Michael <mdorminey79@gmail.com>
2025-10-13 22:48:27 -0400

    Disable PWA on Vercel builds

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
+  disable: process.env.NODE_ENV === 'development' || process.env.VERCEL === '1',
   register: true,
   skipWaiting: true,
 })
